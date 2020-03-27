dbea86567db23a7c2dc7e0455bbcf3c39af7e232
santiago.yacelga <esyacelga@hotmail.com>
2020-03-26 21:23:55 -0500

    Migración de código

--- src/app/pages/geo-config/sector/sector.page.ts
+++ src/app/pages/geo-config/sector/sector.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Sector} from '../../../classes/persona/Sector';
+import {SectorService} from '../../../services/persona/sector.service';
 
 @Component({
   selector: 'app-sector',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SectorPage implements OnInit {
 
-  constructor() { }
+  objSector: Sector;
+  lstSector: Array<Sector>;
 
-  ngOnInit() {
+  constructor(private svrSector: SectorService) {
+
   }
 
+
+  crearNuevo() {
+    this.objSector = new Sector();
+  }
+
+  async obtnerSectores() {
+    // @ts-ignore
+    this.lstSector = await this.svrSector.obtenerSectores();
+  }
+
+  ngOnInit() {
+    this.obtnerSectores();
+  }
+
+  async eliminar(sector: Sector) {
+    sector.estado = 0;
+    await this.svrSector.registarSector(sector);
+    await this.obtnerSectores();
+  }
+
+  async registrar(sector: Sector) {
+    sector.estado = 1;
+    sector.codigo = sector.codigo.toUpperCase();
+    sector.descripcion = sector.descripcion.toUpperCase();
+    await this.svrSector.registarSector(sector);
+    await this.obtnerSectores();
+    this.objSector = null;
+  }
 }
---
+++ src/app/services/persona/sector.service.ts
@@ -0,0 +1,30 @@
+import {Injectable} from '@angular/core';
+import {Sector} from '../../classes/persona/Sector';
+import {CRUD_SECTOR} from '../../constantes/ConstanteTransaccional';
+import {OBTENER_TODOS_SECTOR} from '../../constantes/ConstanteConsulta';
+import {Util} from '../../modules/system/generic/classes/util';
+import {ExecuteCallProcedureService} from '../../modules/system/generic/service/execute-call-procedure.service';
+import {RequestOptions} from '../../modules/system/generic/classes/RequestOptions';
+
+
+@Injectable({
+    providedIn: 'root'
+})
+export class SectorService {
+
+    constructor(private genericService: ExecuteCallProcedureService, private utils: Util) {
+
+    }
+
+    async registarSector(sector: Sector) {
+        const requestOptions = new RequestOptions();
+        return await this.genericService.servicioRestGenericoPost(sector, CRUD_SECTOR, requestOptions) as Sector;
+    }
+
+
+    async obtenerSectores() {
+        const requestOptions = new RequestOptions();
+        return (await this.genericService.servicioRestGenericoGet({}, OBTENER_TODOS_SECTOR, requestOptions)) as Sector[];
+    }
+
+}
